d73b42f8858700643c24cab011e406928dd45d4d
mohammad arif <mdarif.cs@gmail.com>
2019-07-27 09:43:31 +0530

    median of two sorted array

---
+++ ByteByByte/src/main/java/com/example/arrray/MedianOfArrays.java
@@ -0,0 +1,46 @@
+package com.example.arrray;
+
+import java.util.Arrays;
+
+public class MedianOfArrays {
+
+	public static void main(String[] args) {
+		int arr1[] = { 1, 3, 6, 7, 9, 20, 30, 40 };
+		int arr2[] = { 2, 4, 5, 8 };
+		System.out.println("\n\nmedian "+getMedian(arr1, arr2));
+	}
+
+	private static double getMedian(int[] arr1, int[] arr2) {
+		int[] mergArray = mergArray(arr1, arr2);
+		Arrays.stream(mergArray).forEach(e->System.out.print(e+" "));
+		int length = mergArray.length;
+		if(length%2==0) return (mergArray[(length/2)-1] + mergArray[(length/2)])/2.;
+		return mergArray[(length/2)]/2.;
+	}
+
+	private static int[] mergArray(int[] arr1, int[] arr2) {
+		int[] mergArr = new int[arr1.length + arr2.length];
+
+		int i = 0;
+		int j = 0;
+		int k = 0;
+
+		while (i < arr1.length && j < arr2.length) {
+			if(arr1[i]<arr2[j]) 
+				mergArr[k++] = arr1[i++];
+			 else 
+				mergArr[k++] = arr2[j++];
+		}
+		
+		while (i<arr1.length) {
+			mergArr[k++] = arr1[i++];
+		}
+		
+		while (j<arr2.length) {
+			mergArr[k++] = arr2[j++];
+		}
+		
+		return mergArr;
+	}
+
+}
