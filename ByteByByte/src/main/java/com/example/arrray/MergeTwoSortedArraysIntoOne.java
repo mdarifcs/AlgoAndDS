7b0bef86d3ee24558973d06bdc00f5e718963755
mohammad arif <mdarif.cs@gmail.com>
2019-07-30 08:34:08 +0530

    Merge Two Sorted arrays into bigger one

---
+++ ByteByByte/src/main/java/com/example/arrray/MergeTwoSortedArraysIntoOne.java
@@ -0,0 +1,35 @@
+package com.example.arrray;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArraysIntoOne {
+
+	public static void main(String[] args) {
+		int[] arr1 = {1,10,17,0,0,0};
+		int[] arr2 = {14,16,20};
+		mergerArrays(arr1,arr2,3,3);
+		System.out.println(Arrays.toString(arr1));
+	}
+
+	private static void mergerArrays(int[] arr1, int[] arr2, int arr1Length, int arr2Length) {
+		int arr1Index = arr1Length-1;
+		int arr2Index = arr2Length-1;
+		int mainArrayIndex = arr1.length-1;
+		while(arr2Index>=0) {
+			if(arr1[arr1Index]<arr2[arr2Index]) {
+				swipe(arr1,arr2, mainArrayIndex, arr2Index);
+				arr2Index--;
+			} else {
+				swipe(arr1,arr1,arr1Index,mainArrayIndex);
+				arr1Index--;
+			}
+			mainArrayIndex--;
+		}
+	}
+	
+	private static void swipe(int[] arr1, int[] arr2, int arr1Index, int arr2Index) {
+		int tmp = arr1[arr1Index];
+		arr1[arr1Index] = arr2[arr2Index];
+		arr2[arr2Index] = tmp;
+	}
+}
