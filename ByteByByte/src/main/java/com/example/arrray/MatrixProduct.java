08be9bed048a262f9390c11dcc3f0fe591762edb
mohammad arif <mdarif.cs@gmail.com>
2019-08-03 22:49:37 +0530

    MatrixProduct

---
+++ ByteByByte/src/main/java/com/example/arrray/MatrixProduct.java
@@ -0,0 +1,25 @@
+package com.example.arrray;
+
+/**
+ * @author mohammad.arif
+ * Given a matrix, find the path from top left to bottom right with the greatest product
+ * by moving only down and right
+ * 
+ * [1, 2, 3]
+ * [4, 5, 6]
+ * [7, 8, 9]
+ * 
+ * 1->4->7->8->9
+ * 2016
+ * 
+ * [-1, 2,  3]
+ * [4,  5, -6]
+ * [7,  8,  9]
+ * 
+ * -1->4->5->-6->9
+ * 1080
+ */
+
+public class MatrixProduct {
+
+}
