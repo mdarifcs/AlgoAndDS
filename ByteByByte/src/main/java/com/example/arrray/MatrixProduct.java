c2e13287c221a08374abd88bc59e380cedc6eecb
mohammad arif <mdarif.cs@gmail.com>
2019-08-04 10:01:18 +0530

    Only for +ve matrix

--- ByteByByte/src/main/java/com/example/arrray/MatrixProduct.java
+++ ByteByByte/src/main/java/com/example/arrray/MatrixProduct.java
@@ -20,6 +20,28 @@ package com.example.arrray;
  * 1080
  */
 
+//only positive matrix implementation 640000
 public class MatrixProduct {
+	public static void main(String[] args) {
+		int[][] mat = new int[][]  {{1, 10, 80},
+									{4, 5, 20},
+									{7, 8, 40}};
+		System.out.println(getMaxProduct(mat));
+	}
 
+	private static int getMaxProduct(int[][] mat) {
+		int rowLength = mat.length;
+		int colLength = mat[0].length;
+		int[][] result = new int[rowLength][colLength];
+		
+		for (int i = 0; i < rowLength; i++) {
+			for (int j = 0; j < colLength; j++) {
+				if (i==0 && j==0) result[i][j] = mat[i][j];
+				else if (i==0) result[i][j] = mat[i][j-1]*mat[i][j];
+				else if (j==0) result[i][j] = mat[i-1][j]*mat[i][j];
+				else result[i][j] = Math.max(result[i][j-1], result[i-1][j])*mat[i][j];
+			}
+		}
+		return result[rowLength-1][colLength-1];
+	}
 }
