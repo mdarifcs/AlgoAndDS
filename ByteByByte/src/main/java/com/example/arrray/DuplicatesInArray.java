7ca20b014a387ca42c6ea51c7258e7abcf4a0e12
mohammad arif <mdarif.cs@gmail.com>
2019-08-04 09:38:21 +0530

    Duplicates In array Implementations

--- ByteByByte/src/main/java/com/example/arrray/DuplicatesInArray.java
+++ ByteByByte/src/main/java/com/example/arrray/DuplicatesInArray.java
@@ -1,5 +1,8 @@
 package com.example.arrray;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author mohammad.arif
  * Given an array of Integers where each value 1<=x<=len(array)
@@ -8,8 +11,20 @@ package com.example.arrray;
 public class DuplicatesInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = new int[] {1,2,5,4,5,9,1,4,9};
+		System.out.println(findDuplicates(arr));
+	}
 
+	private static Set<Integer> findDuplicates(int[] arr) {
+		Set<Integer> result = new HashSet<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			int elementIndex = Math.abs(arr[i])-1;
+			if(arr[elementIndex]<0)
+				result.add(Math.abs(arr[elementIndex]));
+			else 
+				arr[elementIndex] = -arr[elementIndex];
+		}
+		return result;
 	}
 
 }
