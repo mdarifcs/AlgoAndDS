0bd30fe1f95dd0a263736c374b3e6eb74895ea20
mohammad arif <mdarif.cs@gmail.com>
2019-08-07 18:37:28 +0530

    code implementation

--- ByteByByte/src/main/java/com/example/linkedlist/ReverseLinkedListUsingStack.java
+++ ByteByByte/src/main/java/com/example/linkedlist/ReverseLinkedListUsingStack.java
@@ -1,10 +1,31 @@
 package com.example.linkedlist;
 
+import java.util.Stack;
+
 public class ReverseLinkedListUsingStack {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Node head = Node.createLinkedList(new int[]{1,2,3,4,5,6,7,8,9});
+		Node.printAllNodes(head);
+		Node tail = reverseLinkedList(head);
+		System.out.println("using stack");
+		Node.printAllNodes(tail);
+	}
 
+	private static Node reverseLinkedList(Node head) {
+		Node tail = null;
+		Stack<Node> stack = new Stack<Node>();
+		while (head!=null) {
+			stack.push(new Node(head.data));
+			head = head.next;
+		}
+		tail = stack.pop();
+		Node tmpNode = tail;
+		while (!stack.isEmpty()) {
+			tmpNode.next = stack.pop();
+ 			tmpNode = tmpNode.next;
+		}
+		return tail;
 	}
 
 }
