658acd2e2b4f1c1904811997c2a7316eaced16ee
mohammad arif <mdarif.cs@gmail.com>
2019-08-09 07:56:21 +0530

    Code implementation for kth Most frequent element

--- ByteByByte/src/main/java/com/example/string/sort/KthMostFrequentString.java
+++ ByteByByte/src/main/java/com/example/string/sort/KthMostFrequentString.java
@@ -1,5 +1,10 @@
 package com.example.string.sort;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * @author mohammad.arif
  * Given a list of strings, write a function to find the kth most frequent recurring string
@@ -7,8 +12,32 @@ package com.example.string.sort;
 public class KthMostFrequentString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] str = new String[] {"a","c","a","c","d","b","a"};
+		System.out.println(findMostFrequent(str,3));
+	}
 
+	private static String findMostFrequent(String[] str, int k) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (String s : str) {
+			Integer val = map.get(s);
+			if (val==null) val=0;
+			map.put(s, ++val);
+		}
+		return (map.size()<k)?null:getMostFrequentFromMapUsingQueue(map,k);
+	}
+
+	private static String getMostFrequentFromMapUsingQueue(Map<String, Integer> map, int k) {
+		Queue<Map.Entry<String, Integer>> que = new PriorityQueue<>((o1, o2)-> o1.getValue().compareTo(o2.getValue()));
+		for (Map.Entry<String, Integer> entry : map.entrySet()) {
+			if(k>0) {
+				que.add(entry);
+				k--;
+			} else if (entry.getValue()>que.peek().getValue()) {
+				que.poll();
+				que.add(entry);
+			}
+		}
+		return que.poll().getKey();
 	}
 
 }
