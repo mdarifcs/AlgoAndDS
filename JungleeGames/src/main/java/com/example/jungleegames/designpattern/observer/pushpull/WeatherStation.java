86ce4fc4474b3c8ef6ea4b7b7ac05b853e7f7581
mohammad arif <mdarif.cs@gmail.com>
2019-08-18 10:25:55 +0530

    add xml for observer

--- JungleeGames/src/main/java/com/example/jungleegames/designpattern/observer/pushpull/WeatherStation.java
+++ JungleeGames/src/main/java/com/example/jungleegames/designpattern/observer/pushpull/WeatherStation.java
@@ -5,13 +5,12 @@ public class WeatherStation {
 	public static void main(String[] args) {
 		WeatherData weatherData = new WeatherData();
 		
-		MobileDisplay mobileDisplay = new MobileDisplay(weatherData);
-		WindowDisplay windowDisplay = new WindowDisplay(weatherData);
+		IObserver mobile = new MobileDisplay(weatherData);
+		IObserver window = new WindowDisplay(weatherData);
 		
 		weatherData.setMeasurement(10, 20);
 		System.out.println();
 		weatherData.setMeasurement(100, 200);
-
 	}
 
 }
--- JungleeGames/src/main/java/com/example/jungleegames/designpattern/observer/pushpushsimple/WeatherStation.java
+++ JungleeGames/src/main/java/com/example/jungleegames/designpattern/observer/pushpushsimple/WeatherStation.java
@@ -3,8 +3,8 @@ package com.example.jungleegames.designpattern.observer.pushpushsimple;
 public class WeatherStation {
 	public static void main(String[] args) {
 		WeatherData weatherData = new WeatherData();
-		MobileDisplay mobileDisplay = new MobileDisplay(weatherData);
-		WindowDisplay windowDisplay = new WindowDisplay(weatherData);
+		IObserver mobileDisplay = new MobileDisplay(weatherData);
+		IObserver windowDisplay = new WindowDisplay(weatherData);
 		
 		weatherData.setMeasurement(10, 20);
 		System.out.println();
