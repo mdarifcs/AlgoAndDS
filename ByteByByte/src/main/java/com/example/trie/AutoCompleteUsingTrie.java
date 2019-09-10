f5de2308f8df01ccc57282c23c036c472df5e346
mohammad arif <mdarif.cs@gmail.com>
2019-09-10 08:00:12 +0530

    need to write auto complete using trie

---
+++ ByteByByte/src/main/java/com/example/trie/AutoCompleteUsingTrie.java
@@ -0,0 +1,14 @@
+package com.example.trie;
+
+/**
+ * @author mohammad.arif
+ * write an autocomplete class that returns all dictionary words with a given prefix
+ */
+public class AutoCompleteUsingTrie {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
